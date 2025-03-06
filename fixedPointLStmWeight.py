410730f93de839cca5e2e1adbbad40689d856465
Shakiba <shakiba.bolboliankhah@mail.mcgill.ca>
2025-03-06 13:09:25 -0500

    updated for sigmoid

--- fixedPointLStmWeight.py
+++ fixedPointLStmWeight.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-FRAC_BITS = 3
+FRAC_BITS = 8
 BIT_WIDTH = 16
-MIN = -4
-MAX = 8
+MIN = -8
+MAX = 16
 PAR_DEG = 1
 
 MAX_INT = 2**(BIT_WIDTH-1)
@@ -56,7 +56,7 @@ def sigmoid(x, scale = 2**FRAC_BITS):
     x_div4 = x >> SHIFT_AMOUNT
 
     # Add OFFSET (0.5 in fixed-point)
-    y = x_div4 + OFFSET
+    y = (x_div4 + OFFSET).astype(int_type(BIT_WIDTH))
 
     y_max = np.where(y > 0, y, 0)
     y_clip = np.where(y_max < ONE, y_max, ONE)
@@ -74,7 +74,6 @@ def int_type(N: int):
 
 def fixed_add(a: np.ndarray, b: np.ndarray) -> np.ndarray:
  
-    # Convert to int32 to prevent overflow during addition
     result = a.astype(int_type(BIT_WIDTH*2)) + b.astype(int_type(BIT_WIDTH*2))
     result = v_bankers_round(result, 0)
     
@@ -83,12 +82,11 @@ def fixed_add(a: np.ndarray, b: np.ndarray) -> np.ndarray:
 
 
 def fixed_mul(a: np.ndarray, b: np.ndarray, frac_bits: int = FRAC_BITS) -> np.ndarray:
-    # Multiply as int32 to prevent overflow
+
     product = a.astype(np.int32) * b.astype(int_type(BIT_WIDTH*2))
     # product = product / (2 ** frac_bits)
-    product = v_bankers_round(product) #np.round(product)
+    product = v_bankers_round(product) 
 
-    # product = np.clip(product, MIN_INT, MAX_INT).astype(int_type(BIT_WIDTH))
     return product.astype(int_type(BIT_WIDTH))
 
 
@@ -106,6 +104,7 @@ def fixed_matvec(matrix: np.ndarray, vector: np.ndarray, frac_bits: int = FRAC_B
     acc = np.sum(products, axis=1)
     result = v_bankers_round(acc, 0)
     return result.astype(int_type(BIT_WIDTH))
+
 
 def fixed_matvec_numpy(matrix: np.ndarray, vector: np.ndarray, frac_bits: int = FRAC_BITS) -> np.ndarray:
 
@@ -129,10 +128,9 @@ def generate_random_matrix(row, column, name: str, min_value: int = MIN, max_val
 
 def generate_random_vector(length, name: str, min_value: int = MIN, max_value: int = MAX) -> np.ndarray :
     vector = np.random.randint(min_value, max_value, size=(length, 1))
-    np.savetxt(DIR + name + '.csv', vector.astype(int_type(BIT_WIDTH)), fmt='%i', delimiter=',')
+    np.savetxt(DIR + name + '.csv', vector.astype(int_type(BIT_WIDTH)).T, fmt='%i', delimiter=',')
     
     return vector
-
 
 
 class LSTM:
@@ -153,10 +151,10 @@ class LSTM:
         self.wc = generate_random_matrix(self.hidden_size, self.input_size, "wc")
 
     def generate_biases(self):
-        self.bf = generate_random_vector(self.hidden_size, "bf")
-        self.bi = generate_random_vector(self.hidden_size, "bi")
-        self.bo = generate_random_vector(self.hidden_size, "bo")
-        self.bc = generate_random_vector(self.hidden_size, "bc")
+        self.bf = generate_random_vector(self.hidden_size, "bf").reshape(self.hidden_size,)
+        self.bi = generate_random_vector(self.hidden_size, "bi").reshape(self.hidden_size,)
+        self.bo = generate_random_vector(self.hidden_size, "bo").reshape(self.hidden_size,)
+        self.bc = generate_random_vector(self.hidden_size, "bc").reshape(self.hidden_size,)
 
     def generate_initial_state(self):
         self.h0 = generate_random_vector(self.hidden_size, "h0")
@@ -165,7 +163,7 @@ class LSTM:
         np.savetxt(DIR + 'state' + '.csv', np.concatenate([self.h0, self.c0]).T.astype(int_type(BIT_WIDTH)), fmt='%i', delimiter=',')
 
     def generate_input(self):
-        self.x = generate_random_matrix(self.hidden_units, self.input_size, "x", MIN*2, MAX*2, 1)
+        self.x = generate_random_matrix(self.hidden_units, self.input_size, "x", MIN, MAX, 1)
 
 
     def run_test(self):
@@ -188,20 +186,17 @@ class LSTM:
 
         for x_t in self.x:
 
-            f = sigmoid(fixed_matvec(self.uf, h)+ fixed_matvec(self.wf, x_t))
+            f = sigmoid(fixed_add(fixed_add(fixed_matvec(self.uf, h), fixed_matvec(self.wf, x_t)),self.bf))
 
-            i = sigmoid(fixed_matvec(self.ui, h)+ fixed_matvec(self.wi, x_t))
+            i = sigmoid(fixed_add(fixed_add(fixed_matvec(self.ui, h), fixed_matvec(self.wi, x_t)), self.bi))
 
-            o = sigmoid(fixed_matvec(self.uo, h)+ fixed_matvec(self.wo, x_t))
+            o = sigmoid(fixed_add(fixed_add(fixed_matvec(self.uo, h), fixed_matvec(self.wo, x_t)), self.bo))
 
-            c_prime = tanh(fixed_matvec(self.uc, h)+ fixed_matvec(self.wc, x_t))
+            c_prime = tanh(fixed_add(fixed_add(fixed_matvec(self.uc, h), fixed_matvec(self.wc, x_t)),self.bc))
 
             c = fixed_add(fixed_mul(f, c),fixed_mul(i, c_prime))
 
             h = fixed_mul(tanh(c),o)
-
-            print("c,", c.astype(np.int16))
-            print("h,",h.astype(np.int16))
 
             y += [np.concatenate([h,c])]
 
